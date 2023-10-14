fca601ed454b06c8c4b3035a72685b3949536eae
Baga6312 <magroubertn@gmail.com>
2023-10-14 19:41:57 +0100

    break point

--- db-connection.js
+++ db-connection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 const db = mongoose.connect(process.env.DB_URL, {
    useNewUrlParser: true, 
    useUnifiedTopology: true
-    
 })
+
 if (db) { 
   console.log("connected to database")
 }
--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,52 @@ module.exports = function (app) {
   
     .get(function (req, res){
       let project = req.params.project;
+
+      const { 
+        _id , 
+        open , 
+        issue_title , 
+        issue_text , 
+        created_by , 
+        assgined_to , 
+        status_text , 
+      } = req.body ; 
+
+      Projectmodel.aggregate([
+        { $match: { name: projectName } },
+        { $unwind: "$issues" },
+        _id != undefined
+          ? { $match: { "issues._id": ObjectId(_id) } }
+          : { $match: {} },
+        open != undefined
+          ? { $match: { "issues.open": open } }
+          : { $match: {} },
+        issue_title != undefined
+          ? { $match: { "issues.issue_title": issue_title } }
+          : { $match: {} },
+        issue_text != undefined
+          ? { $match: { "issues.issue_text": issue_text } }
+          : { $match: {} },
+        created_by != undefined
+          ? { $match: { "issues.created_by": created_by } }
+          : { $match: {} },
+        assigned_to != undefined
+          ? { $match: { "issues.assigned_to": assigned_to } }
+          : { $match: {} },
+        status_text != undefined
+          ? { $match: { "issues.status_text": status_text } }
+          : { $match: {} },
+      ])
+      .exec((err, data) => {
+        if (!data) {
+          res.json([]);
+        } else {
+          let mappedData = data.map((item) => item.issues);
+          res.json(mappedData);
+        }
+      })
     })
+    
 
     .post(async (req, res) => {
 
@@ -48,7 +93,7 @@ module.exports = function (app) {
       try {
         await project.save();
         res.status(201).json(newIssue); 
-        console.log("saved to database")
+        console.log("Data saved to database")
       } catch(err) {
         res.status(500).json({error: err.message});
         console.log("error saving to database")
