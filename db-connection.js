0d0c57111a5aa85e85edf6e479bc2cdc86dc48e2
Baga6312 <magroubertn@gmail.com>
2023-10-14 19:01:03 +0100

    removed the .env shit

--- db-connection.js
+++ db-connection.js
@@ -1,26 +1,11 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://oussemabenayech:N6sCI6H5HCinQHzs@cluster0.ovqruar.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+const mongoose = require("mongoose") 
+const db = mongoose.connect(process.env.DB_URL, {
+   useNewUrlParser: true, 
+   useUnifiedTopology: true
+    
+})
+if (db) { 
+  console.log("connected to database")
 }
-run().catch(console.dir);
+
+exports.modules = db ;
