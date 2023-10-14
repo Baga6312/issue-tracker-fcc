8c824be2658c49b70f2dbe5b56f7cfa918922bed
Baga6312 <magroubertn@gmail.com>
2023-10-14 18:16:55 +0100

    still fixing

--- routes/api.js
+++ routes/api.js
@@ -11,82 +11,34 @@ module.exports = function (app) {
     .get(function (req, res){
       let project = req.params.project;
     })
-    
-    // .post(function (req, res){
-    //   let project = req.params.project;
-    //   const { 
-    //     issue_title , 
-    //     issue_text , 
-    //     created_by , 
-    //     assigned_to , 
-    //     status_text ,
-    //   }=req.body; 
 
-    //   if(!issue_title || !issue_text || !created_by ){ 
-    //     req.json({error : "required field(s) missing"})
-    //     return ; 
-    //   }
-
-    //   const newIssue = new Issuemodel ( { 
-    //     issue_title: issue_title || "" , 
-    //     issue_text: issue_text || "" , 
-    //     created_on : new Date() , 
-    //     update_on : new Date() ,
-    //     created_by : created_by || "" , 
-    //     assigned_to : assigned_to || "", 
-    //     open : true , 
-    //     status_text : status_text || "" , 
-    //   })
-
-    //  const data = Projectmodel.findOne({name : project} , (err , projectdata)=> {  
-    //     if(!data) {
-    //       const newProject = new Projectmodel({name : project} , ) ; 
-    //       newProject.Issues.push(newIssue) ;
-    //       newProject.save((err , data )=> { 
-    //         if (err || !data) { 
-    //           res.send("there was an error saving in post") 
-    //           console.log("error saving in post")
-    //         }else{ 
-    //           res.json(newIssue)
-    //         }
-    //       })
-    //     }
-    //     else {
-    //       projectdata.Issues.push(newIssue) ;
-    //       projectdata.save((err , data ) => { 
-    //         if ( err || !data) { 
-    //           res.send("there was error saving in post") 
-    //           console.log("error saving in post") 
-    //         }else{
-    //           res.json(newIssue)
-    //         }
-    //       })
-    //     }
-    //   })
-    // } )
-    
     .post(async (req, res) => {
 
-      const { issue_title, issue_text, created_by } = req.body;
-    
+      const { issue_title , issue_text , created_by , assigned_to , status_text , } = req.body;
+
       if(!issue_title || !issue_text || !created_by) {
         return res.status(400).json({error: "Required fields missing"});
       }
-    
-      const newIssue = new Issuemodel({
-        title: issue_title, 
-        text: issue_text,
-        createdBy: created_by
-      });
-    
+     const newIssue = new Issuemodel ( { 
+        issue_title: issue_title || "" , 
+        issue_text: issue_text || "" , 
+        created_on : new Date() , 
+        update_on : new Date() ,
+        created_by : created_by || "" , 
+        assigned_to : assigned_to || "", 
+        open : true , 
+        status_text : status_text || "" , 
+      })
+
       let project;
       
       try {
         project = await Projectmodel.findOne({name: req.params.project});
       } catch(err) {
         return res.status(500).json({error: err.message}); 
+       
       }
-    
+ 
       if(!project) {
         project = new Projectmodel({name: req.params.project});
       }
@@ -96,36 +48,12 @@ module.exports = function (app) {
       try {
         await project.save();
         res.status(201).json(newIssue); 
+        console.log("saved to database")
       } catch(err) {
         res.status(500).json({error: err.message});
+        console.log("error saving to database")
       }
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
   
     app.put(function (req, res){
       let project = req.params.project;
